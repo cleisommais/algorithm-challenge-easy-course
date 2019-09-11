f787dff056ac17a77b2953e8a16ea6af1ae1f8de
Cleison Ferreira de Melo <cleisommais@gmail.com>
2019-09-11 19:21:48 +0100

    Finished 2D Array - DS
    Signed-off-by: Cleison Ferreira de Melo <cleisommais@gmail.com>

--- src/main/java/com/algorithms/array_and_strings/TwoDArrayDS.java
+++ src/main/java/com/algorithms/array_and_strings/TwoDArrayDS.java
@@ -1,7 +1,22 @@
 package com.algorithms.array_and_strings;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TwoDArrayDS {
     public int hourglassSum(int[][] arr) {
-        return 0;
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = 0; j < arr[0].length - 2; j++) {
+                int firstLine = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+                int secondLine = arr[i + 1][j + 1];
+                int thirdLine = arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                int sum = firstLine + secondLine + thirdLine;
+                temp.add(sum);
+            }
+        }
+        Collections.sort(temp);
+        return temp.get(temp.size() - 1);
     }
 }
--- src/test/java/com/algorithms/array_and_strings/TwoDArrayDSTest.java
+++ src/test/java/com/algorithms/array_and_strings/TwoDArrayDSTest.java
@@ -7,30 +7,7 @@ public class TwoDArrayDSTest {
     @Test
     public void hourglassSum() {
         TwoDArrayDS twoD = new TwoDArrayDS();
-        int[][] arr = new int[6][6];
-        arr[0][0] = 1;
-        arr[0][1] = 1;
-        arr[0][2] = 1;
-        arr[0][3] = 0;
-        arr[0][4] = 0;
-        arr[0][5] = 0;
-        arr[1][0] = 0;
-        arr[2][1] = 1;
-        arr[3][2] = 0;
-        arr[4][3] = 0;
-        arr[5][4] = 0;
-        //arr[6][5] = 0;
-
-
-        String[] arrRowItems = {"1", "1", "1", "0", "0", "0"};
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                int arrItem = Integer.parseInt(arrRowItems[j]);
-                //arr[i][j] = arrItem;
-                System.out.print(arr[i][j] + ", ");
-            }
-            System.out.println();
-        }
+        int[][] arr = {{1, 1, 1, 0, 0, 0}, {0, 1, 0, 0, 0, 0}, {1, 1, 1, 0, 0, 0}, {0, 0, 2, 4, 4, 0}, {0, 0, 0, 2, 0, 0}, {0, 0, 1, 2, 4, 0}};
 
         Assert.assertEquals(19, twoD.hourglassSum(arr));
     }
