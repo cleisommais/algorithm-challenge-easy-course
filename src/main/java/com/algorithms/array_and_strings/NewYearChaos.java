822984f4a83b2e802923f61ef1f43480dce7cd08
Cleison Ferreira de Melo <cleisommais@gmail.com>
2019-10-29 21:15:54 +0000

    NewYearChaos test
    Signed-off-by: Cleison Ferreira de Melo <cleisommais@gmail.com>

--- src/main/java/com/algorithms/array_and_strings/NewYearChaos.java
+++ src/main/java/com/algorithms/array_and_strings/NewYearChaos.java
@@ -1,7 +1,25 @@
 package com.algorithms.array_and_strings;
 
 public class NewYearChaos {
-    public int minimumBribes(int[] q) {
-        return 0;
+
+    public void minimumBribes(int[] q) {
+        int size = q.length;
+        int bribe = 0;
+        int count = 0;
+        for (int i = size - 1; i >= 0; i--) {
+            if (q[count] - (count + 1) > 2) {
+                System.out.println("Too chaotic");
+                break;
+            }
+            count++;
+            int j = 0;
+            while (j <= i) {
+                if (q[j] > q[i]) {
+                    bribe++;
+                }
+                j++;
+            }
+        }
+        System.out.println(bribe);
     }
 }
